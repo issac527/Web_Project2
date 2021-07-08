6a958d0ce4cb46d5210af53ee3b7934bc9c8489d
Issac <lim_527@naver.com>
2021-07-08 15:12:05 +0900

    2021-07-08 HTTP PROTOCOL GET, POST METHOD

--- acountapp/views.py
+++ acountapp/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def render_base(re):
-    return render(re, 'acountapp/hello_world.html')
+    if re.method == 'POST' :
+        return render(re, 'acountapp/hello_world.html',
+                      context={"text" : "POST METHOD"})
+    else:
+        return render(re, 'acountapp/hello_world.html',
+                      context={"text" : "GET METHOD"})
 
 def render_test(re):
     return render(re, 'acountapp/hello_world_2.html')
