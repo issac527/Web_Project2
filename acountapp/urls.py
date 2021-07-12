ee2a40b0179e6f4219eb96e0ed77a361d3ca9743
Issac <lim_527@naver.com>
2021-07-12 16:43:36 +0900

    2021-07-12 CreateView Class, Html Create

--- acountapp/urls.py
+++ acountapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from acountapp.views import render_base, render_test
+from acountapp.views import render_base, render_test, AccountCreateView
 
 app_name = 'acountapp'
 
 urlpatterns = [
     path('rb/', render_base, name = "rb"),
-    path('rt/', render_test, name = "rt")
+    path('rt/', render_test, name = "rt"),
+    path("create/", AccountCreateView.as_view(), name = "create")
 ]
--- acountapp/views.py
+++ acountapp/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+
+from django.views.generic import CreateView
 
 # Create your views here.
 from acountapp.models import HelloWorld
-
+from django.urls import reverse, reverse_lazy
 
 def render_base(re):
     if re.method == 'POST' :
@@ -12,10 +16,8 @@ def render_base(re):
         n_hw = HelloWorld()
         n_hw.text = temp
         n_hw.save()
-        HelloWorld_list = HelloWorld.objects.all()
 
-        return render(re, 'acountapp/hello_world.html',
-                      context={"HelloWorld_list" : HelloWorld_list})
+        return HttpResponseRedirect(reverse("acountapp:rb"))
     else:
         HelloWorld_list = HelloWorld.objects.all()
         return render(re, 'acountapp/hello_world.html',
@@ -23,3 +25,10 @@ def render_base(re):
 
 def render_test(re):
     return render(re, 'acountapp/hello_world_2.html')
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # 클래스 내에서 reverse시 reverse_lazy로 사용
+    success_url = reverse_lazy("acountapp:create")
+    template_name = "acountapp/create.html"
