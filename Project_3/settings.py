90b8ebbe95346fdb9fc81726755ff32d9e43c96a
Issac <lim_527@naver.com>
2021-07-29 15:18:15 +0900

    2021-07-29 articleapp Create and Settings

--- Project_3/settings.py
+++ Project_3/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'acountapp',
     'profileapp',
+    'articleapp',
 ]
 
 MIDDLEWARE = [
--- Project_3/urls.py
+++ Project_3/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ac/', include('acountapp.urls')),
-    path('pr/', include('profileapp.urls'))
+    path('pr/', include('profileapp.urls')),
+    path('at/', include('articleapp.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
