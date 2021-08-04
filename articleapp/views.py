2b9a1dcbdd654cbe19ff435da1efc7c3e0e568c1
Issac <lim_527@naver.com>
2021-08-04 09:48:42 +0900

    2021-08-04 ArticleUpdateView, update.html ArticleDeleteView, delete.html Create

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -22,3 +23,19 @@ class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    # success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk' : self.object.pk})
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy("articleapp:list")
+    template_name = 'articleapp/delete.html'
