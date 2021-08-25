a8a1bfa0c54514dd23da557f663dc3d59646bc0b
Issac <lim_527@naver.com>
2021-08-25 10:05:41 +0900

    2021-08-25 Django Messages Update

--- likeapp/views.py
+++ likeapp/views.py
@@ -8,6 +8,7 @@ from django.views.generic import RedirectView
 from django.urls import reverse
 from articleapp.models import Article
 from likeapp.models import LikeRecord
+from django.contrib import messages
 
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
@@ -22,16 +23,14 @@ class LikeArticleView(RedirectView):
 
         # 좋아요 기록이 존재 시 좋아요 과정없이 해당 페이지로 이동
         if likeRecord.exists():
-            # if article.like > 0:
-            #     article.like -= 1
-            #     article.save()
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
+            messages.add_message(request, messages.ERROR, "좋아요는 한번만 가능합니다.")
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
-
+        messages.add_message(request, messages.SUCCESS, "좋아요가 반영되었습니다.")
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
