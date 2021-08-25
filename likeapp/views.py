af8d29510b0a1d8e6a60f2a9433daef5484d8093
Issac <lim_527@naver.com>
2021-08-25 10:30:30 +0900

    2021-08-25 LikeArticleView Update transaction.atomic -> db_transaction Method

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -10,6 +12,20 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 from django.contrib import messages
 
+@transaction.atomic
+def db_transaction(user, article):
+
+    likeRecord = LikeRecord.objects.filter(user=user,
+                                           article=article)
+
+    article.like += 1
+    article.save()
+
+    if likeRecord.exists():
+        raise ValidationError("좋아요가 이미 존재합니다.") # 개발자가 확인하는 에러 메시지
+    else:
+        LikeRecord(user=user, article=article).save()
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
@@ -18,19 +34,13 @@ class LikeArticleView(RedirectView):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        likeRecord = LikeRecord.objects.filter(user=user,
-                                               article=article)
-
-        # 좋아요 기록이 존재 시 좋아요 과정없이 해당 페이지로 이동
-        if likeRecord.exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(request, messages.SUCCESS, "좋아요가 반영되었습니다.")
+        except ValidationError:
             messages.add_message(request, messages.ERROR, "좋아요는 한번만 가능합니다.")
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
 
-        article.like += 1
-        article.save()
-        messages.add_message(request, messages.SUCCESS, "좋아요가 반영되었습니다.")
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
